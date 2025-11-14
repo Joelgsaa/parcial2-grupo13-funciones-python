ed57fc11a5d753c23a791ddfbcba7e87901006ec
Joel Saavedra <joelgsaavedra@gmail.com>
2025-11-14 15:48:46 -0300

    Estructura inicial del proyecto

---
+++ main.py
@@ -0,0 +1,6 @@
+#Archivo principal del grupo
+#Cada integrante deberá agregar su función en un archivo independiente
+from funciones import *
+
+if name == "main":
+    print("Ejecución de pruebas de las funciones del grupo")
