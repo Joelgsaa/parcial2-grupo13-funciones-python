49c04c88794c6de3d1025e697ced59e8d4bd96fe
fackumaretl-crypto <facku.maretl@gmail.com>
2025-11-14 16:50:42 -0300

    Update martel.py

--- funciones/martel.py
+++ funciones/martel.py
@@ -2,10 +2,3 @@ def multiplicacion_martel(a, b):
     """Devuelve el producto de dos números."""
 
     return a * b
-
-
-def multiplicar(a, b):
-    """
-    Función requerida por el test ya que no me funcionaba.
-    """
-    return a * b
