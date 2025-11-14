624717a6d1c108809a4417f779f763a8085b1851
fackumaretl-crypto <facku.maretl@gmail.com>
2025-11-14 16:41:52 -0300

    test_martel.py

--- tests/test_martel.py
+++ tests/test_martel.py
@@ -3,3 +3,5 @@ from funciones.martel import multiplicacion_martel
 
 def test_multiplicacion_martel():
     assert multiplicacion_martel(3, 5) == 15
+    assert multiplicacion_martel(3, 4) == 12
+    assert multiplicacion_martel(-2, 5) == -10
