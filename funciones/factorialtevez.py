b961ce7579063ae5eac999fd8584d13d7411a238
ramiroYB <ramiro.ybanez2003@gmail.com>
2025-11-14 17:21:20 -0300

    arregle mis funciones

--- funciones/factorialtevez.py
+++
@@ -1,7 +0,0 @@
-def factorial(n):
-    \"\"\"Calcula factorial de n (iterativo).\"\"\" 
-        raise ValueError('n debe ser un entero no negativo') 
-    result = 1 
-    for i in range(2, n+1): 
-        result *= i 
-    return result 
